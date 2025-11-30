34a1287cfccf84b0b80fe003a7c7f649da3c8162
Isratjahan19 <isratjahan2k219@gmail.com>
2025-12-01 01:53:37 +0600

    Israt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { SessionProvider } from './contexts/SessionContext';
 import { AppRouter } from './router';
-
+// added by Israt
 function App() {
   return (
     <BrowserRouter>
