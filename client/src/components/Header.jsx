47d5679e0f072a9145421da22869d675b49be754
AnkitaD76 <ankita.dey@g.bracu.ac.bd>
2025-12-19 21:12:12 +0600

    fix: update link in Header component from Settings to Wallet

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -130,11 +130,11 @@ export const Header = () => {
                       </Link>
 
                       <Link
-                        to="/settings"
+                        to="/wallet"
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                         onClick={() => setIsUserMenuOpen(false)}
                       >
-                        Settings
+                        Wallet
                       </Link>
 
                       <button
